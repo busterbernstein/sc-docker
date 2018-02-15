df7d485e5b5f26b16b513609f7a1e552124f4c74
Buster <busterbernstein@users.noreply.github.com>
2018-02-15 22:52:44 +0100

    Update publish.sh

--- docker/publish.sh
+++ docker/publish.sh
@@ -4,20 +4,12 @@ set -eux
 # do not publish starcraft:game
 VERSION=$(python ../setup.py --version)
 
-docker tag starcraft:wine  ggaic/starcraft:wine
-docker tag starcraft:bwapi ggaic/starcraft:bwapi
-docker tag starcraft:java  ggaic/starcraft:java
-docker tag starcraft:play  ggaic/starcraft:play
-docker tag starcraft:wine  ggaic/starcraft:wine-${VERSION}
-docker tag starcraft:bwapi ggaic/starcraft:bwapi-${VERSION}
-docker tag starcraft:java  ggaic/starcraft:java-${VERSION}
-docker tag starcraft:play  ggaic/starcraft:play-${VERSION}
+docker tag starcraft:wine  bbernstein/copycatsc:wine
+docker tag starcraft:bwapi bbernstein/copycatsc:bwapi
+docker tag starcraft:java  bbernstein/copycatsc:java
+docker tag starcraft:play  bbernstein/copycatsc:play
 
-docker push ggaic/starcraft:wine
-docker push ggaic/starcraft:bwapi
-docker push ggaic/starcraft:java
-docker push ggaic/starcraft:play
-docker push ggaic/starcraft:wine-${VERSION}
-docker push ggaic/starcraft:bwapi-${VERSION}
-docker push ggaic/starcraft:java-${VERSION}
-docker push ggaic/starcraft:play-${VERSION}
+docker push bbernstein/copycatsc:wine
+docker push bbernstein/copycatsc:bwapi
+docker push bbernstein/copycatsc:java
+docker push bbernstein/copycatsc:play
